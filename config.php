56e1f0b58246b746fe0d07888e807ff0b26139ef
Amal142 <158220433+Amal142@users.noreply.github.com>
2025-05-21 23:42:39 +0000

    Update config.php

--- config.php
+++ config.php
@@ -3,6 +3,10 @@ $host = "localhost";
 $username = "root";   // ou ton nom d'utilisateur MySQL
 $dbname = "tourisme"; // ✅ Nom exact de ta base de données
 $password = "";       // mot de passe (souvent vide sous XAMPP)
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'tourisme');
+define('DB_USER', 'root');
+define('DB_PASS', '');
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
